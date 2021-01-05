880b51a00e510c3d3ed9fa6459af0c1d38984a0c
sushidesu <hugemerge@gmail.com>
2021-01-05 18:18:28 +0900

    課題3: サーバーの実装、テストコードの追加

--- 3/task_3_tomoki_ishihara/src/app.js
+++ 3/task_3_tomoki_ishihara/src/app.js
@@ -2,10 +2,6 @@ const express = require("express")
 const app = express()
 const PORT = 8000
 
-app.get("/", (_, response) => {
-  response.send({ text: "hello world" })
-})
-
 app.listen(PORT, () => {
   console.log(`listening at http://localhost:${PORT}`)
 })
