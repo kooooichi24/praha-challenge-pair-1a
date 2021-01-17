ffc430b57adaaffd00f25fe073c495ba09871558
sushidesu <hugemerge@gmail.com>
2021-01-17 16:50:08 +0900

    課題5: クッキーセット用のルートを用意

--- 5/task_5_tomoki_ishihara/src/anotherServer.js
+++ 5/task_5_tomoki_ishihara/src/anotherServer.js
@@ -35,9 +35,41 @@ server.use(
   })
 )
 
-// / -> 受け取ったクッキーを表示する
+// /set-cookies -> クッキーセット用
+server.get("/set-cookies", (_, res) => {
+  res
+    .cookie("3rdParty", "cookie", {
+      httpOnly: true,
+    })
+    .cookie("3rdPartySameSiteStrict", "cookie", {
+      httpOnly: true,
+      sameSite: "strict",
+    })
+    .cookie("3rdPartySameSiteLax", "cookie", {
+      httpOnly: true,
+      sameSite: "lax",
+    })
+    .cookie("3rdPartySameSiteNone", "cookie", {
+      httpOnly: true,
+      sameSite: "none",
+    })
+    .cookie("3rdPartySameSiteSecureNone", "cookie", {
+      httpOnly: true,
+      sameSite: "none",
+      secure: true,
+    })
+    .send("<p>Cookies set.</p>")
+})
+
+// / -> 受け取ったクッキーを表示する（クッキーのセットは行わない）
 server.get("/", (req, res) => {
-  res.send(`<pre>${JSON.stringify(req.cookies, undefined, 2)}</pre>`)
+  res.send(
+    `<div><p><a href="/">same-site request</a></p><pre>${JSON.stringify(
+      req.cookies,
+      undefined,
+      2
+    )}</pre></div>`
+  )
 })
 
 export default server
