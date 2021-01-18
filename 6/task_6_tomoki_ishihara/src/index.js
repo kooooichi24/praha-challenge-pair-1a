8b5f69d10ca88c0c9b2398cd6fc3464ae99d5380
sushidesu <hugemerge@gmail.com>
2021-01-18 21:25:50 +0900

    課題6: サーバを実装

--- 6/task_6_tomoki_ishihara/src/index.js
+++ 6/task_6_tomoki_ishihara/src/index.js
@@ -1,1 +1,20 @@
-console.log("what is CORS?")
+import ngrok from "ngrok"
+import api from "./apiServer"
+import server from "./server"
+
+const PORT = 8000
+const API_PORT = 9000
+
+api.listen(API_PORT, () => {
+  console.log(`api server at localhost:${API_PORT} ...`)
+})
+
+!(async () => {
+  const apiURL = await ngrok.connect(API_PORT)
+  console.log(`localhost:${API_PORT}} on ${apiURL}`)
+
+  server.locals.apiURL = apiURL
+  server.listen(PORT, () => {
+    console.log(`server at localhost:${PORT} ...`)
+  })
+})()
