b29457d22f02f1fbe98d3d44632655a6327d1a96
古川広一 <ex.kouichi1224@gmail.com>
2021-01-22 23:59:58 +0900

    Fix: 課題6_3_実装

--- 6/task_6_furukawa/cors-mock/origin-b/index.js
+++ 6/task_6_furukawa/cors-mock/origin-b/index.js
@@ -2,25 +2,21 @@ const express = require('express');
 const app = express();
 const port = 3001;
 
-const BASE_URL = 'http://87cf0c721b07.ngrok.io'
+const ACCESS_CONTROL_ALLOW_ORIGIN = 'http://87cf0c721b07.ngrok.io'
 
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }));
 
 /* -------------------------------------------------------------------------- */
 
-app.head('/simple-cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', BASE_URL);
-  res.sendStatus(204).json();
+
+app.get('simple-cors', (req, res) => {
+  res.set('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN);
+  res.json({ message: 'Simple CORS requests are working. [GET]' })
 });
 
-app.get('simple-cors', (req, res => {
-  res.set('Access-Control-Allow-Origin', BASE_URL);
-  res.json({ message: 'Simple CORS requests are working. [GET]' })
-}));
-
 app.post('/simple-cors', (req, res) => {
-  res.set('Access-Control-Allow-Origin', BASE_URL);
+  res.set('Access-Control-Allow-Origin', ACCESS_CONTROL_ALLOW_ORIGIN);
   res.json({ message: 'Simple CORS requests are working. [POST]' });
 });
 
@@ -28,14 +24,14 @@ app.post('/simple-cors', (req, res) => {
 
 app.options("/complex-cors", (req, res) => {
   res.set({
-    'Access-Control-Allow-Origin': BASE_URL,
+    'Access-Control-Allow-Origin': ACCESS_CONTROL_ALLOW_ORIGIN,
     'Access-Control-Allow-Methods': 'POST, GET, OPTIONS',
     'Access-Control-Allow-Headers': 'X-PINGOTHER, Content-Type',
   });
   res.json();
 });
 app.post('/complex-cors', (req, res) => {
-  res.set({ 'Access-Control-Allow-Origin': BASE_URL })
+  res.set({ 'Access-Control-Allow-Origin': ACCESS_CONTROL_ALLOW_ORIGIN })
   res.json({ message: 'Complex CORS requests are working. [POST]' });
 });
 
