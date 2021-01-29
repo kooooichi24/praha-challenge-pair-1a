d252125c7c32a13afb02f9f4bfc07d90a9f8f2b5
sushidesu <hugemerge@gmail.com>
2021-01-29 19:38:15 +0900

    課題7: キャッシュのサンプルを追加

--- 7/task_7_tomoki_ishihara/src/server.js
+++ 7/task_7_tomoki_ishihara/src/server.js
@@ -17,7 +17,7 @@ server.use(
   })
 )
 server.use(
-  "/no-cache",
+  "/no-store",
   Express.static(path.join(__dirname, "public"), {
     setHeaders: (res) => {
       res.header("Cache-Control", "no-store, max-age=0")
@@ -25,4 +25,41 @@ server.use(
   })
 )
 
+server.use(
+  "/no-cache",
+  Express.static(path.join(__dirname, "public"), {
+    setHeaders: (res) => {
+      res.header("Cache-Control", "no-cache")
+    },
+  })
+)
+
+server.use(
+  "/must-revalidate",
+  Express.static(path.join(__dirname, "public"), {
+    setHeaders: (res) => {
+      res.header("Cache-Control", "must-revalidate")
+    },
+  })
+)
+
+server.use(
+  "/maxage-0",
+  Express.static(path.join(__dirname, "public"), {
+    setHeaders: (res) => {
+      res.header("Cache-Control", "max-age=0")
+    },
+  })
+)
+
+server.use(
+  "/expires-0",
+  Express.static(path.join(__dirname, "public"), {
+    setHeaders: (res) => {
+      res
+        .header("Expires", "-1")
+        .header("Cache-Control", null)
+    },
+  })
+)
 export default server
