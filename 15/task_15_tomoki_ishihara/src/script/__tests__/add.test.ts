3a232da8d14d06f36613e2b38c47f2c1719817e4
sushidesu <hugemerge@gmail.com>
2021-03-05 20:04:31 +0900

    テストの構造家 (テストとTODOリストの文言を修正)

--- 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
@@ -1,7 +1,7 @@
 import { add } from "../add";
 
 describe("add関数は引数を全て加算して結果を返す", () => {
-  describe("引数を全て加算する", () => {
+  describe("結果が1000以下の場合 結果をそのまま返す", () => {
     it("1, 1 を受け取ると 2 を返す", () => {
       expect(add(1, 1)).toBe(2);
     });
@@ -9,7 +9,7 @@ describe("add関数は引数を全て加算して結果を返す", () => {
       expect(add(1, 1, 1)).toBe(3);
     });
   });
-  describe(`計算結果が1000を超える場合は "too big" を返す`, () => {
+  describe(`結果が1000より大きい場合は "too big" を返す`, () => {
     it(`1001 を渡すと "too big" を返す`, () => {
       expect(add(1001)).toBe("too big");
     });
