29b382f50c310f08f8cd06fd7370693ef3af9a82
sushidesu <hugemerge@gmail.com>
2021-03-05 17:44:48 +0900

    add-1-1: Refactor (アサーションをインライン化)

--- 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
@@ -3,8 +3,8 @@ import { add } from "../add";
 describe("add関数は引数を全て加算して結果を返す", () => {
   describe("引数を全て加算する", () => {
     it("1, 1 を受け取ると 2 を返す", () => {
-      const actual = add(1, 1);
-      expect(actual).toBe(2);
+      // 実行 & 検証
+      expect(add(1, 1)).toBe(2);
     });
   });
 });
