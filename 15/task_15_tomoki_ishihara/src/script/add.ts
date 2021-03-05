3ee09bc68d457cbb8eb9a60780864fa6d96fa3e2
sushidesu <hugemerge@gmail.com>
2021-03-05 19:32:54 +0900

    add-2-2: Refactor

--- 15/task_15_tomoki_ishihara/src/script/add.ts
+++ 15/task_15_tomoki_ishihara/src/script/add.ts
@@ -1,7 +1,7 @@
 export const add = (...numbers: number[]): string | number => {
   const TOO_BIG = "too big";
   const result = numbers.reduce((prev, cur) => prev + cur, 0);
-  if (result > 1000) {
+  if (1000 < result) {
     return TOO_BIG;
   } else {
     return result;
