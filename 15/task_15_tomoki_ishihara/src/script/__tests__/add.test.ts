d1f97ff2d5a826757d1a42e15b626cdd5a021048
sushidesu <hugemerge@gmail.com>
2021-03-05 19:20:50 +0900

    add-2-1: Red (`too big` のテストを追加)

--- 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
@@ -11,4 +11,10 @@ describe("add関数は引数を全て加算して結果を返す", () => {
       expect(add(1, 1, 1)).toBe(3);
     });
   });
+  describe(`計算結果が1000を超える場合は "too big" を返す`, () => {
+    it(`1001 を渡すと "too big" を返す`, () => {
+      // 実行 & 検証
+      expect(add(1001)).toBe("too big");
+    });
+  });
 });
