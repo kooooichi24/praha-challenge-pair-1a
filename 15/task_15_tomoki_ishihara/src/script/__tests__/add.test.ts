5feb6f3c47fb770c0ad32debdbfc0fe6466ec017
sushidesu <hugemerge@gmail.com>
2021-03-05 19:26:54 +0900

    add-2-1: Refactor ("too big" を定数化)

--- 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
@@ -3,17 +3,14 @@ import { add } from "../add";
 describe("add関数は引数を全て加算して結果を返す", () => {
   describe("引数を全て加算する", () => {
     it("1, 1 を受け取ると 2 を返す", () => {
-      // 実行 & 検証
       expect(add(1, 1)).toBe(2);
     });
     it("1, 1, 1 を受け取ると 3 を返す", () => {
-      // 実行 & 検証
       expect(add(1, 1, 1)).toBe(3);
     });
   });
   describe(`計算結果が1000を超える場合は "too big" を返す`, () => {
     it(`1001 を渡すと "too big" を返す`, () => {
-      // 実行 & 検証
       expect(add(1001)).toBe("too big");
     });
   });
--- 15/task_15_tomoki_ishihara/src/script/add.ts
+++ 15/task_15_tomoki_ishihara/src/script/add.ts
@@ -1,7 +1,8 @@
 export const add = (...numbers: number[]): string | number => {
+  const TOO_BIG = "too big";
   const result = numbers.reduce((prev, cur) => prev + cur, 0);
   if (result > 1000) {
-    return "too big";
+    return TOO_BIG;
   } else {
     return result;
   }
