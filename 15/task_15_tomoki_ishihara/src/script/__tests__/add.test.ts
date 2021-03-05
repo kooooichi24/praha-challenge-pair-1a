b404b4c40c2313de8edb4ae0b7a6b9b3e0d5ea63
sushidesu <hugemerge@gmail.com>
2021-03-05 17:32:36 +0900

    add-1-1: Red (テストが落ちた)

--- 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/add.test.ts
@@ -1,7 +1,9 @@
+import { add } from "../add";
+
 describe("add関数は引数を全て加算して結果を返す", () => {
   describe("引数を全て加算する", () => {
     it("1, 1 を受け取ると 2 を返す", () => {
-      let actual;
+      const actual = add(1, 1);
       expect(actual).toBe(2);
     });
   });
