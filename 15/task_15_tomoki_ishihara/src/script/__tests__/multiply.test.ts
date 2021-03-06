a3a50f28cf55edac315f8cdf7ce9791dc34bbeac
sushidesu <hugemerge@gmail.com>
2021-03-06 14:04:59 +0900

    multiply-1-2: Green

--- 15/task_15_tomoki_ishihara/src/script/__tests__/multiply.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/multiply.test.ts
@@ -5,5 +5,8 @@ describe("multiply 関数は引数を全て乗算して結果を返す", () => {
     it("2, 3 を渡すと 6 を返す", () => {
       expect(multiply(2, 3)).toBe(6);
     });
+    it("2, 3, 0 を渡すと 0 を返す", () => {
+      expect(multiply(2, 3, 0)).toBe(0);
+    });
   });
 });
