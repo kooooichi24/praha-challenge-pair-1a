116dfdb6ea61c70ef7c3098cccd6c5ded67b382f
sushidesu <hugemerge@gmail.com>
2021-03-06 14:42:59 +0900

    divide-1-1: Refactor

--- 15/task_15_tomoki_ishihara/src/script/divide.ts
+++ 15/task_15_tomoki_ishihara/src/script/divide.ts
@@ -1,4 +1,4 @@
-export const divide = (...numbers: number[]) => {
+export const divide = (...numbers: number[]): number => {
   const first = numbers[0];
   const rest = numbers.slice(1);
   return rest.reduce((prev, cur) => prev / cur, first);
