e1fa1757698f5262c2639ad299ce25f963e65ce9
sushidesu <hugemerge@gmail.com>
2021-03-06 11:10:37 +0900

    sub-2-1: Refactor

--- 15/task_15_tomoki_ishihara/src/script/subtract.ts
+++ 15/task_15_tomoki_ishihara/src/script/subtract.ts
@@ -1,9 +1,10 @@
 export const subtract = (...numbers: number[]): string | number => {
+  const NEGATIVE_NUMBER = "negative number";
   const first = numbers[0];
   const rest = numbers.slice(1, numbers.length);
   const result = rest.reduce((prev, cur) => prev - cur, first);
   if (result < 0) {
-    return "negative number";
+    return NEGATIVE_NUMBER;
   } else {
     return result;
   }
