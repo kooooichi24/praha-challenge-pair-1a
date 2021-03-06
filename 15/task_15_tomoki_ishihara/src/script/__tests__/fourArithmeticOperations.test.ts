9d052ce338e97cff4638d7d45146cce9101c7057
sushidesu <hugemerge@gmail.com>
2021-03-06 15:54:48 +0900

    fix

--- 15/task_15_tomoki_ishihara/src/script/__tests__/fourArithmeticOperations.test.ts
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/fourArithmeticOperations.test.ts
@@ -3,7 +3,7 @@ import { add } from "../add";
 
 describe("fourArithmeticOperationsクラスは四則演算を実行する", () => {
   describe("exec関数は渡された引数に対して計算を行い結果を返す", () => {
-    describe("先頭の引数 (multiply/add/subtract/divide) をみて、対応する計算を実行する", () => {
+    describe("先頭の引数 (任意の文字列) をみて、対応する計算を実行する", () => {
       describe("先頭が add の場合 add関数を実行する", () => {
         it(`"add", 1, 1 を渡すと 2を返す`, () => {
           const fourArithmeticOperations = new FourArithmeticOperations({
