8f44a8529c5ffbf13b5240475d360a07fee83d57
sushidesu <hugemerge@gmail.com>
2021-03-06 14:10:49 +0900

    multiply-2-1: Refactor

--- 15/task_15_tomoki_ishihara/src/script/multiply.ts
+++ 15/task_15_tomoki_ishihara/src/script/multiply.ts
@@ -1,7 +1,8 @@
 export const multiply = (...numbers: number[]): number | string => {
+  const BIG_BIG_NUMBER = "big big number";
   const result = numbers.reduce((prev, cur) => prev * cur, 1);
   if (result > 1000) {
-    return "big big number";
+    return BIG_BIG_NUMBER;
   } else {
     return result;
   }
