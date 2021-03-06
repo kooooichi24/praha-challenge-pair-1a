9c7ccadd8d4086fb2bedf720dde49e3172e389f1
sushidesu <hugemerge@gmail.com>
2021-03-06 13:59:45 +0900

    multiply-1-1: Red

---
+++ 15/task_15_tomoki_ishihara/src/script/__tests__/multiply.test.ts
@@ -0,0 +1,9 @@
+import { multiply } from "../multiply";
+
+describe("multiply 関数は引数を全て乗算して結果を返す", () => {
+  describe("計算結果が1000以下の場合は結果をそのまま返す", () => {
+    it("2, 3 を渡すと 6 を返す", () => {
+      expect(multiply(2, 3)).toBe(6);
+    });
+  });
+});
---
+++ 15/task_15_tomoki_ishihara/src/script/multiply.ts
@@ -0,0 +1,3 @@
+export const multiply = (...numbers: number[]) => {
+  // TODO
+};
