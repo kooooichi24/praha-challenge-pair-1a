5502b25dbb31708ea2a617c9d42fd9ac12fa8ae2
sushidesu <hugemerge@gmail.com>
2021-03-06 14:10:16 +0900

    multiply-2-1: Green

--- 15/task_15_tomoki_ishihara/src/script/multiply.ts
+++ 15/task_15_tomoki_ishihara/src/script/multiply.ts
@@ -1,3 +1,8 @@
-export const multiply = (...numbers: number[]): number => {
-  return numbers.reduce((prev, cur) => prev * cur, 1);
+export const multiply = (...numbers: number[]): number | string => {
+  const result = numbers.reduce((prev, cur) => prev * cur, 1);
+  if (result > 1000) {
+    return "big big number";
+  } else {
+    return result;
+  }
 };
