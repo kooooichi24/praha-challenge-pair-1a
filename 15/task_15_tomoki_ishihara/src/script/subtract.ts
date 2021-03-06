05b2b1739242b4155c347eaf3c7d80bf17a2a303
sushidesu <hugemerge@gmail.com>
2021-03-06 11:22:27 +0900

    sub-3-1: Green

--- 15/task_15_tomoki_ishihara/src/script/subtract.ts
+++ 15/task_15_tomoki_ishihara/src/script/subtract.ts
@@ -1,5 +1,8 @@
 export const subtract = (...numbers: number[]): string | number => {
   const NEGATIVE_NUMBER = "negative number";
+  if (numbers.length === 0) {
+    return 0;
+  }
   const first = numbers[0];
   const rest = numbers.slice(1, numbers.length);
   const result = rest.reduce((prev, cur) => prev - cur, first);
