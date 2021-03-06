d255a99ee8ed9ac57f37ccb2359b0e3e4d17eaa6
sushidesu <hugemerge@gmail.com>
2021-03-06 15:40:13 +0900

    divide-3-1: Green

--- 15/task_15_tomoki_ishihara/src/script/divide.ts
+++ 15/task_15_tomoki_ishihara/src/script/divide.ts
@@ -1,5 +1,8 @@
 export const divide = (...numbers: number[]): number => {
   const N_1e10 = 10000000000;
+  if (numbers.length === 0) {
+    return 0;
+  }
   const first = numbers[0];
   const rest = numbers.slice(1);
   const result = rest.reduce((prev, cur) => prev / cur, first);
