742efea5d29ef0c04bd794bfb3f56da3fc049b4c
sushidesu <hugemerge@gmail.com>
2021-03-06 16:51:44 +0900

    four-invalid_numbers: Green

--- 15/task_15_tomoki_ishihara/src/script/fourArithmeticOperations.ts
+++ 15/task_15_tomoki_ishihara/src/script/fourArithmeticOperations.ts
@@ -6,6 +6,7 @@ interface Operators {
 }
 
 const NUMBERS_1_30_ERROR = Error("Numbers length must be in the range 1-30.");
+const NUMBERS_MUST_NUMBER_ERROR = Error("Numbers must be number.");
 const INVALID_OPERATION_ERROR = Error(
   "Operation must be one of multiply, add, subtract, divide."
 );
@@ -16,22 +17,33 @@ export class FourArithmeticOperations {
   constructor(operators: Operators) {
     this.operators = operators;
   }
-  exec(operation?: string, ...numbers: number[]): number | string | undefined {
+  exec(
+    operation?: string,
+    ...numbers: (string | number)[]
+  ): number | string | undefined {
     if (operation === undefined) {
       throw OPERATION_REQUIRED_ERROR;
     }
     if (numbers.length === 0 || 30 < numbers.length) {
       throw NUMBERS_1_30_ERROR;
     }
+    const numbersConverted = numbers.map((mayBeNum) => {
+      const num = parseFloat(mayBeNum.toString());
+      if (Number.isNaN(num)) {
+        throw NUMBERS_MUST_NUMBER_ERROR;
+      } else {
+        return num;
+      }
+    });
     switch (operation) {
       case "add":
-        return this.operators.add(...numbers);
+        return this.operators.add(...numbersConverted);
       case "multiply":
-        return this.operators.multiply(...numbers);
+        return this.operators.multiply(...numbersConverted);
       case "subtract":
-        return this.operators.subtract(...numbers);
+        return this.operators.subtract(...numbersConverted);
       case "divide":
-        return this.operators.divide(...numbers);
+        return this.operators.divide(...numbersConverted);
       default:
         throw INVALID_OPERATION_ERROR;
     }
